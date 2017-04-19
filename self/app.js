145ee9429aca8e096bd82e4da588431213944678
hansjuergnett <goeginett@hotmail.com>
2017-04-19 17:40:23 +0200

    Init Webapp

--- self/app.js
+++ self/app.js
@@ -1,3 +1,9 @@
 /**
  * Created by Nett on 11.04.2017.
  */
+'use strict'
+import Controller from './controller'
+
+let ctr = new Controller(window.document)
+
+
--- self/controller.js
+++ self/controller.js
@@ -1,3 +1,21 @@
 /**
  * Created by Nett on 18.04.2017.
  */
+
+'use strict'
+
+export default class {
+
+    constructor($doc){
+        this.$doc = $doc
+        let $input = $doc.querySelector("#inputtxt")
+        $input.addEventListener("change", this.onChangeInput.bind(this))
+    }
+
+    onChangeInput(ev){
+        let $list = this.$doc.querySelector("#list")
+        let $li = this.$doc.createElement("li")
+        $li.innerText = ev.target.value
+        $list.appendChild($li)
+    }
+}
